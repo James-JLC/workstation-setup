041a0b9a17d4d6745247a195d566fccb1cffd468
jilichao <3123790+jilichao@users.noreply.github.com>
2020-07-27 12:00:20 +0800

    Create k8s-setup.sh

---
+++ k8s-setup.sh
@@ -0,0 +1,17 @@
+sudo su root
+cat <<EOF >/etc/apt/sources.list.d/docker-k8s.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable
+EOF
+
+apt-get update && apt-get install -y docker-ce kubelet kubeadm kubectl
+
+swapoff -a
+
+for i in `kubeadm config images list`; do
+    imageName=${i#k8s.gcr.io/}
+    echo "docker pull hub-mirror.c.163.com/kubesphere/$imageName"
+    docker pull hub-mirror.c.163.com/kubesphere/$imageName
+    docker tag hub-mirror.c.163.com/kubesphere/$imageName k8s.gcr.io/$imageName
+    docker rmi hub-mirror.c.163.com/kubesphere/$imageName
+done;
