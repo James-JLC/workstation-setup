04cbea11fd249adf2d5882f7ce4644f3337de442
jilichao <3123790+jilichao@users.noreply.github.com>
2020-07-27 12:29:36 +0800

    Update k8s-setup.sh

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -15,3 +15,10 @@ for i in `kubeadm config images list`; do
     docker tag hub-mirror.c.163.com/kubesphere/$imageName k8s.gcr.io/$imageName
     docker rmi hub-mirror.c.163.com/kubesphere/$imageName
 done;
+
+for i in `kubeadm config images list`; do
+　　imageName=${i#k8s.gcr.io/}
+　　docker pull registry.aliyuncs.com/google_containers/$imageName
+　　docker tag registry.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
+　　docker rmi registry.aliyuncs.com/google_containers/$imageName
+done;
